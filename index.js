d9f813c6e107009a6bb765405446a0ffbbe2be41
Mélanie DEMOUSSIS <melaniedemoussis@MacBook-Air-de-Melanie.local>
2023-02-16 15:11:05 +0100

    correction skip

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/characters", async (req, res) => {
     const limit = req.query.limit || "100";
 
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&name=${name}&skip=${slip}&limit=${limit}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&name=${name}&skip=${skip}&limit=${limit}`
     );
     console.log(response.data);
     res.status(200).json(response.data);
